3bb4c259711f7bfbc52864ee7b1268fe97f36b4b
LighthouseKeeperYN <freesaber@yandex.ru>
2019-09-25 13:16:54 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  let hoursNeeded = knowsProgramming ? 800 : 800 + 500;
+  let hoursPerWeek = config[focus];
+
+  return Math.ceil(hoursNeeded / hoursPerWeek);
+};
